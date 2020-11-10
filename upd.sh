58807ed245527b518aed9b4ef324a491d5cd1834
xax <xa@universe>
2020-11-10 22:41:57 +0100

    Use fast-forwards only in update script upd.sh.

--- upd.sh
+++ upd.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-cVersion="1.1.0"
+cVersion="1.1.1"
 cCopyright="Copyright (C) XA, III 2020. All rights reserved."
 cName="${0##*/}"
 declare -r cName cVersion cCopyright
@@ -9,7 +9,7 @@ upd_sub_repo () {
   local repo="$1"
   if [[ -n $repo && -d "$repo" ]]; then
     printf "* Updating sub repository \"$repo\"...\n"
-    pushd "$repo" && git pull \
+    pushd "$repo" && git pull --ff-only \
     && git log --since="12 hours ago" --abbrev-commit --date="format-local:%Y-%m-%d %H:%M%z"  --pretty="tformat:%C(auto)%h %C(cyan)%cd %C(auto)%s"
     popd
     printf "+ Done.\n\n"
